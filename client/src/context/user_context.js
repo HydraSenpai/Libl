8947b9de7a375260d1b9d3008107d31f7c56cd58
nicholas <nicholas.daly.2020@strath.ac.uk>
2023-08-18 12:53:41 +0100

    created user context

---
+++ client/src/context/user_context.js
@@ -0,0 +1,24 @@
+import React, { useContext, useEffect, useReducer, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const UserContext = React.createContext();
+const UserProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <UserContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUserContext = () => {
+  return useContext(UserContext);
+};
+
+export { UserProvider, useUserContext, initialState };
