233c40e4ea623ef3cb7dc1765401eb6f9188f2a6
Ahmed Rhouma <ahmedousama5@gmail.com>
2023-08-20 11:34:40 +0100

    added book section carousel

--- client/src/components/BookSection.jsx
+++ client/src/components/BookSection.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import BookCard from '../components/BookCard'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import '@splidejs/splide/dist/css/splide.min.css'
 
 const BookSection = ({ heading, books }) => {
   return (
@@ -7,12 +9,24 @@ const BookSection = ({ heading, books }) => {
       <div className='book-sec-title'>
         <h3 className='row-header'>{heading}</h3>
       </div>
-      <div className='book-sec-row'>
-        <div className='container'>
+
+      <div className='books-container'>
+        <Splide
+          options={{
+            perPage: 5,
+            arrows: false,
+            pagination: false,
+            drag: 'free',
+            autoWidth: true,
+            gap: '2rem',
+          }}
+        >
           {books.map((book) => (
-            <BookCard key={book._id} book={book} />
+            <SplideSlide>
+              <BookCard key={book._id} book={book} />
+            </SplideSlide>
           ))}
-        </div>
+        </Splide>
       </div>
     </div>
   )
