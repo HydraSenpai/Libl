f033704dccfd006187ce27b5382e4e9e65a33a6f
Ahmed Rhouma <ahmedousama5@gmail.com>
2023-08-23 09:20:09 +0100

    did some of book page

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
+const styles = {
+  cardImg: {
+    width: '130px',
+    height: '100%',
+  },
+}
+
 const BookCard = ({ book }) => {
   return (
     <Card className='book-card'>
-      <Link to={`/book/:id/${book._id}`}>
+      <Link to={`/book/${book._id}`}>
         <Card.Body className='book-card-body'>
+          <Card.Img variant='top' src={book.cover} style={styles.cardImg} />
           <Card.Title as='div' className='book-card-title'>
             <strong>{book.book_title}</strong>
           </Card.Title>
--- client/src/pages/Book.jsx
+++ client/src/pages/Book.jsx
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import BookSection from '../components/BookSection'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 const Book = () => {
-  return <div>Book</div>;
-};
+  const { id: bookId } = useParams()
+  const [book, setBook] = useState([])
+  const [author, setAuthor] = useState('')
 
-export default Book;
+  useEffect(() => {
+    try {
+      axios
+        .get(`http://localhost:4000/api/v1/books/${bookId}`)
+        .then((response) => {
+          setBook(response.data.book)
+          setAuthor(
+            response.data.book.authors.author.first_name +
+              ' ' +
+              response.data.book.authors.author.second_name
+          )
+        })
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  console.log(book)
+
+  return (
+    <>
+      <Navbar />
+      <div className='bookpage-hero'>
+        <div className='bookpage-book-details'>
+          <h3>{book.book_title}</h3>
+          <p>
+            <strong>Author: </strong> {author}
+          </p>
+          <p>
+            <strong>Release Date: </strong>
+            {book.book_release_date}
+          </p>
+        </div>
+      </div>
+      <div className='bookpage-summary-div'>
+        <div className='bookpage-summary-header'>
+          <h4>Summary</h4>
+          <p>{book.book_description}</p>
+        </div>
+        <div className='bookpage-summary-details'>
+          <h4>Details</h4>
+          <ul className='bookpage-details-list'>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default Book
