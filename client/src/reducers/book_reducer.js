ff6d5313c9184a6b05135d7df2a6f0d2bacf6076
nicholas <nicholas.daly.2020@strath.ac.uk>
2023-08-24 15:28:02 +0100

    get all books frontend context function

--- client/src/reducers/book_reducer.js
+++ client/src/reducers/book_reducer.js
@@ -1,4 +1,31 @@
+import {
+  GET_BOOKS_BEGIN,
+  GET_BOOKS_SUCCESS,
+  GET_BOOKS_ERROR,
+} from '../actions/book_actions';
+
 const book_reducer = (state, action) => {
-  return { ...state };
+  if (action.type === GET_BOOKS_BEGIN) {
+    return {
+      ...state,
+      isLoading: true,
+    };
+  }
+  if (action.type === GET_BOOKS_SUCCESS) {
+    return {
+      ...state,
+      books: action.payload.books,
+      totalBooks: action.payload.numOfBooks,
+      isLoading: false,
+    };
+  }
+  if (action.type === GET_BOOKS_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+    };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
+
+export default book_reducer;
